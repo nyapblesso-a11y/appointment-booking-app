b5568c59510ab0edbd92a29cb4c70042e51db419
nyapblesso-a11y <nyapblesso@gmail.com>
2026-02-03 11:24:45 +0100

    the env file ad the env.js file created implementing all the neccessary environmental variables fron the database end point

---
+++ config/db.js
@@ -0,0 +1,23 @@
+// import pkg from 'pg'
+// import { env } from './env.js'
+
+// const {Pool} = pkg
+
+// export const pool = new Pool ({
+//  port: env.DB_PORT,
+//  host: env.DB_HOST,
+//  database: env.DB_NAME,
+//  user: env.DB_USER,
+//  password: env.DB_PASSWORD
+// })
+
+// export const connectDB = async () => {
+//   try {
+//     const client = await pool.connect();
+//     console.log('PostgreSQL connected');
+//     client.release();
+//   } catch (error) {
+//     console.error(' PostgreSQL connection failed:', error);
+//     process.exit(1);
+//   }
+// } 
--- config/jwt.js
+++ config/jwt.js
@@ -1,1 +1,12 @@
+// import jwt from 'jsonwebtoken';
+// import { env } from './env.js';
 
+// export const signToken = (payload) => {
+//   return jwt.sign(payload, env.JWT_SECRET, {
+//     expiresIn: env.JWT_EXPIRES_IN
+//   });
+// };
+
+// export const verifyToken = (token) => {
+//   return jwt.verify(token, env.JWT_SECRET);
+// };
