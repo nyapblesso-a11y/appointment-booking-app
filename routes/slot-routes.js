8ee4dd9fadd61bccef31f07e053f9717434f3edb
nyapblesso-a11y <nyapblesso@gmail.com>
2026-02-04 11:53:50 +0100

    the routes for appoitments has been implemented

--- routes/slot-routes.js
+++ routes/slot-routes.js
@@ -1,8 +1,28 @@
 import express from 'express';
+import {
+  createSlot,
+  getProviderSlots
+} from '../controllers/slot.controller.js';
+
+import { authenticate } from '../middleware/auth.middleware.js';
+import { authorizeRole } from '../middleware/role.middleware.js';
+
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send('index');
-});
+// Provider creates time slots
+router.post(
+  '/',
+  authenticate,
+  authorizeRole('provider'),
+  createSlot
+);
+
+// Provider views their slots
+router.get(
+  '/',
+  authenticate,
+  authorizeRole('provider'),
+  getProviderSlots
+);
 
 export default router;
