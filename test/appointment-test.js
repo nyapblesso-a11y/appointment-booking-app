41575ec1469af433e866a9d165837e5393e90281
nyapblesso-a11y <nyapblesso@gmail.com>
2026-02-05 14:19:30 +0100

    the test setup has been implemented

---
+++ test/appointment-test.js
@@ -0,0 +1,12 @@
+import { pool } from '../config/db.js';
+
+beforeAll(async () => {
+  await pool.query('DELETE FROM appointments');
+  await pool.query('DELETE FROM slots');
+  await pool.query('DELETE FROM providers');
+  await pool.query('DELETE FROM users');
+});
+
+afterAll(async () => {
+  await pool.end();
+});
