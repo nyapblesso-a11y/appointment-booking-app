bbc2a1a402ce837ef0d6d2304b93b41d51a79459
nyapblesso-a11y <nyapblesso@gmail.com>
2026-02-07 11:54:07 +0100

    the login test, the slot test, the appointment test has been implemented and ready to be tested

---
+++ test/appointTest/appointment.test.js
@@ -0,0 +1,33 @@
+import request from 'supertest';
+import app from '../../app.js';
+
+let clientToken;
+let slotId;
+
+beforeAll(async () => {
+  const login = await request(app)
+    .post('/auth/login')
+    .send({
+      email: 'client@test.com',
+      password: 'password123'
+    });
+
+  clientToken = login.body.token;
+
+  const slots = await request(app)
+    .get('/slot');
+
+  slotId = slots.body.slots[0].id;
+});
+
+describe('Appointments', () => {
+  test('client books appointment', async () => {
+    const res = await request(app)
+      .post('/app')
+      .set('Authorization', `Bearer ${clientToken}`)
+      .send({ slotId });
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body.appointment).toBeDefined();
+  });
+});
