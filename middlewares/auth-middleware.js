737387915a00a98af0421b5a3d2076b20cb9c4bd
nyapblesso-a11y <nyapblesso@gmail.com>
2026-02-10 11:14:25 +0100

    logger implementaton  has been made at the level of auth  middleware

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,23 +1,26 @@
-import { verifyToken } from '../config/jwt.js';
+import { verifyToken } from "../config/jwt.js";
+import logger from "../utils/logger.js";
 
 export const authenticate = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    logger.warn("Authenticaton required");
     return res.status(401).json({
-      message: 'Authentication required',
+      message: "Authentication required",
     });
   }
 
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
 
   try {
     const decoded = verifyToken(token);
-    req.user = decoded; 
+    req.user = decoded;
     next();
   } catch (error) {
+    logger.warn('Invalid or expired token')
     return res.status(401).json({
-      message: 'Invalid or expired token',
+      message: "Invalid or expired token",
     });
   }
 };
