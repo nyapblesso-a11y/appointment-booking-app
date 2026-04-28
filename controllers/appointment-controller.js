6d2660303d3f42390342b596ac4b0e70e2870c10
nyapblesso-a11y <nyapblesso@gmail.com>
2026-04-28 12:00:30 +0100

    the appointment controller has been fix and touhches made

--- controllers/appointment-controller.js
+++ controllers/appointment-controller.js
@@ -4,7 +4,6 @@ import {
   getProviderAppointments,
   cancelUserAppointment,
 } from '../services/appointment-services.js';
-import { logger } from '../utils/logger.js';
 
 // POST /app
 export const createAppointment = async (req, res, next) => {
@@ -12,32 +11,31 @@ export const createAppointment = async (req, res, next) => {
     const { slotId } = req.body;
 
     if (!slotId) {
-      return res.status(400).json({ message: 'slotId is required' });
+      return res.status(400).json({
+        message: 'slotId is required'
+      });
     }
 
     const appointment = await bookAppointment(req.user.id, slotId);
 
-    return res.status(201).json({
+    res.status(201).json({
       success: true,
       appointment
     });
-
   } catch (error) {
-    console.error("APPOINTMENT ERROR:", error);
-    return res.status(500).json({
-      message: error.message
-    });
+    next(error);
   }
 };
-
-
-
 
 // GET /app/client
 export const viewClientAppointments = async (req, res, next) => {
   try {
     const appointments = await getClientAppointments(req.user.id);
-    res.json({ success: true, appointments });
+
+    res.json({
+      success: true,
+      appointments
+    });
   } catch (error) {
     next(error);
   }
@@ -47,7 +45,11 @@ export const viewClientAppointments = async (req, res, next) => {
 export const viewProviderAppointments = async (req, res, next) => {
   try {
     const appointments = await getProviderAppointments(req.user.id);
-    res.json({ success: true, appointments });
+
+    res.json({
+      success: true,
+      appointments
+    });
   } catch (error) {
     next(error);
   }
@@ -57,11 +59,18 @@ export const viewProviderAppointments = async (req, res, next) => {
 export const cancelAppointment = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const appointment = await cancelUserAppointment(req.user.id, id, req.user.role);
-      logger.info('Appoimtment cancelled')
 
-    res.json({ success: true, message: 'Appointment canceled', appointment });
-    
+    const appointment = await cancelUserAppointment(
+      req.user.id,
+      id,
+      req.user.role
+    );
+
+    res.json({
+      success: true,
+      message: 'Appointment canceled',
+      appointment
+    });
   } catch (error) {
     next(error);
   }
