d7823bfa4c6202b35017fd693c201b751d29ed07
nyapblesso-a11y <nyapblesso@gmail.com>
2026-05-07 15:10:07 +0100

    the databse base connections between render ans postesgrel has been qadjsuted

--- config/db.js
+++ config/db.js
@@ -1,26 +1,34 @@
-import pkg from 'pg'
-import { env } from './env.js'
+import pkg from "pg";
+import { env } from "./env.js";
 
-const {Pool} = pkg
+const { Pool } = pkg;
 
-export const pool = new Pool ({
- port: env.DB_PORT,
- host: env.DB_HOST,
- database: env.DB_NAME,
- user: env.DB_USER,
- password: env.DB_PASSWORD,
-})
-
+export const pool = new Pool(
+  env.DATABASE_URL
+    ? {
+        connectionString: env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: env.DB_HOST,
+        port: Number(env.DB_PORT || 5432),
+        user: env.DB_USER,
+        password: env.DB_PASSWORD,
+        database: env.DB_NAME,
+      }
+);
 
 export const connectDB = async () => {
   try {
     const client = await pool.connect();
-    console.log('PostgreSQL connected:', env.DB_NAME);
+
+    console.log("✅ PostgreSQL connected");
+
     client.release();
   } catch (error) {
-    console.error(' PostgreSQL connection failed:', error);
+    console.error("❌ PostgreSQL connection failed:", error);
     process.exit(1);
   }
-} 
+};
 
-export default pool
+export default pool;
--- config/env.js
+++ config/env.js
@@ -1,15 +1,18 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const env = {
   PORT: process.env.PORT || 3000,
+
+  DATABASE_URL: process.env.DATABASE_URL, 
+
   DB_HOST: process.env.DB_HOST,
   DB_PORT: process.env.DB_PORT || 5432,
   DB_NAME: process.env.DB_NAME,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
-  JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1d',
-};
 
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1d",
+};
